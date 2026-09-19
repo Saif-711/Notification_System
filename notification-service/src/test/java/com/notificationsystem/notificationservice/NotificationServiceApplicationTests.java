5c81a72cc0ee58fca85eed108d83117ccac7135a
Saif-711 <saifnairat23@gmail.com>
2026-09-19 03:44:20 +0100

    remove folder not needed

--- notification-service/src/test/java/com/notificationsystem/notificationservice/NotificationServiceApplicationTests.java
+++
@@ -1,8 +0,0 @@
-package com.notificationsystem.notificationservice;
-
-/**
- * Default Spring Boot test class.
- * We do not start the full app here yet, because the real app needs Kafka + MySQL running.
- */
-class NotificationServiceApplicationTests {
-}
